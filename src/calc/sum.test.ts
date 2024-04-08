8d9f5cb20902481ec4afdb66b8f949e74b69655b
hamru <goo.hiro.w@gmail.com>
2024-04-08 20:38:14 +0900

    Parameterized test

--- src/calc/sum.test.ts
+++ src/calc/sum.test.ts
@@ -3,3 +3,11 @@ import { sum } from "./sum";
 it("Calculate the sum of yesterday's weight and today's weight.", () => {
     expect(sum(72, 71)).toBe(143);
 })
+
+it.each`
+    a | b | expected
+    ${1} | ${2} | ${3}
+    ${-1} | ${-2} | ${-3}
+`("Adding a and b results in expected.", ({ a, b, expected }) => {
+    expect(sum(a, b)).toBe(expected);
+});
